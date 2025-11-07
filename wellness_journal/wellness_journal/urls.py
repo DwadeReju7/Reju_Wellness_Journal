aa2e698da9b7036abaec87294698f1e9332d21d2
Dante' Wade <dantewade@Dantes-Laptop.local>
2025-11-07 16:20:15 -0500

    Set up server and created home, admin, and api routes

--- wellness_journal/wellness_journal/urls.py
+++ wellness_journal/wellness_journal/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Welcome to the Wellness Journal App")
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('api/', include('wellness_journal.urls')),
+    path('api/', include('journal.urls')),
 ]
